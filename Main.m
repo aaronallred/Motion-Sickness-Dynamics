c42d515b5564310aaab33ed734978d0303105f30
aaronallred <88723734+aaronallred@users.noreply.github.com>
2023-04-10 11:34:31 -0600

    Changed to Cian Example

--- Main.m
+++ Main.m
@@ -3,31 +3,86 @@
 % Date: 3/3/2023
 clc;clear;
 
-%% Define motion profile length
-TotalTime = 60*60; % seconds
-t0 = 0.1; % model sim sec
-
-%initialization
-dt = t0; %sec
-model_time = (t0:dt:TotalTime)';
-model_motion = [0 0 0 0 0 0].*model_time; 
+% Set to 'Custom' for a custom profile
+% Set to 'Cian' for Cian et al. (2011) OVAR motion
+% Set to 'Gravity Transition' for a 1g to 0g trans. w/o adaptation
+motiontype = 'Cian';
 
 %% Define Motion Profile
-freq = 0.3;
-amp = 3.6 /((2*pi*freq)^2); % zposition amplitude
 
-% Model Motion for simulation
-% columns 1-3 are xyz position
-% columns 4-6 are xyz rotations
-model_motion(:,2) = amp*sin(freq*2*pi*(model_time));
-% Glevel is the Gravity Environment
-Glevel = 1;
+switch motiontype
+    case 'Custom'
+        % Define motion profile length
+        TotalTime = 60*60; % seconds
+        t0 = 0.1; % model sim sec
 
-% freq = 0.3;
-% amp = 0*200 /((2*pi*freq)); % wx amplitude
-% model_motion = [0 0 0 0 0 0].*model_time; %initialize 
-% model_motion(:,4) = amp*cos(freq*2*pi*(model_time));
-% Glevel = 0.00;
+        %initialization
+        dt = t0; %sec
+        model_time = (t0:dt:TotalTime)';
+        model_motion = [0 0 0 0 0 0].*model_time; 
+
+        % Model Motion for simulation
+        % columns 1-3 are xyz position
+        % columns 4-6 are xyz rotations
+        freq = 0.3;
+        amp = 3.6 /((2*pi*freq)^2); % zposition amplitude
+        model_motion(:,2) = amp*sin(freq*2*pi*(model_time));
+        
+        % Glevel is the Gravity Environment
+        Glevel = 1;
+
+        tidx = 1:round(1/freq/dt*3); % time indeces to plot
+
+    case 'Gravity Transition'
+        freq = 0.3;
+        amp = 0*200 /((2*pi*freq)); % wx amplitude
+        model_motion = [0 0 0 0 0 0].*model_time; %initialize 
+        model_motion(:,4) = amp*cos(freq*2*pi*(model_time));
+        Glevel = 0.00;
+
+        tidx = 1:round(1/freq/dt*3); % time indeces to plot
+        
+    case 'Cian'
+        % Cian Simulation
+        T = 60*20+102; %seconds
+        dt = 0.1;
+        model_time = (0:dt:T)';
+        model_motion = [0 0 0 0 0 0].*zeros(length(model_time),1);
+        
+        starttime = 102;
+        endtime = 122;
+        ang = 12; %degrees
+        
+        RotSpeed = ang/(endtime-starttime);
+        % Human Reference Frame motions
+        YawSpeed = model_time*(1); YawSpeed(YawSpeed >72) = 72;
+        YawAngle = cumtrapz(model_time,YawSpeed);
+        RollSpeed = model_time*0;
+        PitchSpeed = model_time*0;
+        
+        RvE = [1;0;0]; % tilt vector in Earth system
+        
+        for i = 1:length(model_time)
+            
+            if model_time(i) > starttime && model_time(i) < endtime
+                Rx = [1 0 0;0 cosd(ang) -sind(ang);0 sind(ang) cosd(ang)];
+                Rz = [cosd(YawAngle(i)) -sind(YawAngle(i)) 0;sind(YawAngle(i)) cosd(YawAngle(i)) 0; 0 0 1];
+                RvH = (Rz\(Rx\RvE))';
+                model_motion(i,4:6) = [RollSpeed(i) PitchSpeed(i) YawSpeed(i)]+...
+                                      RvH*RotSpeed;
+            else
+                model_motion(i,4:6) = [RollSpeed(i) PitchSpeed(i) YawSpeed(i)];
+            end
+        end
+        
+        spindowntime = 10;%sec
+        recovery = (model_time(end)+dt:dt:model_time(end)+20*60)';
+        recovery_motion = linspace(model_motion(end,6),0,spindowntime/dt)';
+        model_time = [model_time;recovery];
+        model_motion = [model_motion;zeros(length(recovery_motion),5) recovery_motion;zeros(length(recovery)-length(recovery_motion),6)];
+
+        tidx = 1:length(model_time);
+end
 
 %% Observer + Oman Motion Sickness Dynamics    
 [ts, Ru,h,hcomp] = RunObserver(model_time,model_motion,Glevel);
@@ -39,8 +94,7 @@ R = cont2MISC(R);
 
 %% Plot
 FS = 20; % FontSize
-tidx = 1:round(1/freq/dt*3); % time indeces to plot
-cd = colormap('jet');
+cd = colormap('hsv');
 
 legMotion = {'X-axis Position (m/s^2)',...
              'Y-axis Position (m/s^2)',...
